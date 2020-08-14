1ae24f525cca426db115c415376696cfac91ddc2
眺望流浪的时光 <xurui0898@163.com>
2020-08-14 18:01:55 +0800

    logback日志文件配置

--- server-api/src/main/java/com/boilerplate/server/controller/IndexController.java
+++ server-api/src/main/java/com/boilerplate/server/controller/IndexController.java
@@ -4,6 +4,8 @@ import com.boilerplate.server.dao.AccountMapper;
 import com.boilerplate.server.dao.TestMapper;
 import com.boilerplate.server.model.AccountExample;
 import com.boilerplate.server.model.TestExample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
     @Autowired
     private TestMapper testMapper;
 
@@ -21,6 +25,7 @@ public class IndexController {
 
     @RequestMapping("/")
     public Object index() {
+        LOGGER.info("[info]请求index...");
         TestExample testExample = new TestExample();
         testExample.setLimitStart(1);
         testExample.setLimitEnd(1);
@@ -29,6 +34,7 @@ public class IndexController {
 
     @RequestMapping("account")
     public Object account() {
+        LOGGER.info("[info]请求account...");
         AccountExample accountExample = new AccountExample();
         return accountMapper.selectByExample(accountExample);
     }
