8c58d0693cf735a4a39040f102270c94b793b027
眺望流浪的时光 <xurui0898@163.com>
2021-09-27 20:40:45 +0800

    无重复字符的最长子串

--- server-api/src/main/java/com/boilerplate/server/controller/LeetCodeController.java
+++ server-api/src/main/java/com/boilerplate/server/controller/LeetCodeController.java
@@ -3,6 +3,7 @@ package com.boilerplate.server.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 @RestController
@@ -10,9 +11,12 @@ import java.util.HashMap;
 public class LeetCodeController {
     public static void main(String[] args) {
         log.info("code test");
+        String s = "abcabeddcbb";
+        int num = lengthOfLongestSubstring(s);
+        System.out.println("num = " + num);
     }
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         int[] indexData = new int[2];
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -24,5 +28,21 @@ public class LeetCodeController {
             hashMap.put(target - nums[i], i);
         }
         return indexData;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        int left = 0,max = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
     }
 }
