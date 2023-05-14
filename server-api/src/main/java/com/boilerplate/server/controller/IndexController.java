dbe1b5adc083fb6ef8c0d8d91160ddce65215221
眺望流浪的时光 <xurui0898@163.com>
2023-05-14 18:46:57 +0800

    新增TestService

--- server-api/src/main/java/com/boilerplate/server/controller/IndexController.java
+++ server-api/src/main/java/com/boilerplate/server/controller/IndexController.java
@@ -1,16 +1,8 @@
 package com.boilerplate.server.controller;
 
-import com.google.common.collect.Lists;
+import com.boilerplate.server.service.TestService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 默认controller
@@ -18,64 +10,13 @@ import java.util.stream.Collectors;
 @RestController
 @Slf4j
 public class IndexController {
+
     public static void main(String[] args) {
         //日期时间格式化与时间戳转换
-        timeHandle();
+        TestService.datetimeHandle();
+        //StringUtils工具类 字符串处理
+        TestService.stringHandle();
         //正确删除list中的元素
-        removeList();
-        //使用工具类进行字符串拼接
-        String s1 = null;
-        String s2 = "椰树椰汁·坚持在海南岛用新鲜椰肉鲜榨";
-        System.out.println(StringUtils.join(s1,s2));
-    }
-
-    /**
-     * 演示：如何正确删除list的元素
-     * 禁止在foreach时删除元素，会报错
-     */
-    public static void removeList() {
-        List<String> list = Lists.newArrayList("北京市","海淀区","上地","清河");
-        String delStr = "海淀区";
-        System.out.println("原数据："+list);
-        //错误方法：禁止在foreach时删除元素，会报错
-        /*for (String s : list) {
-            if (delStr.equals(s)) {
-                list.remove(s);
-            }
-        }*/
-        //正确方法1
-        Iterator<String> it = list.iterator();
-        while (it.hasNext()) {
-            String s = it.next();
-            if (delStr.equals(s)) {
-                it.remove();
-            }
-        }
-        //正确方法2
-        list = list.stream().filter(f-> !f.contains(delStr)).collect(Collectors.toList());
-        //正确方法3
-        list.removeIf(s-> s.equals(delStr) || s.equals("北京市"));
-        System.out.println("新数据："+list);
-    }
-
-    /**
-     * 日期时间格式化与时间戳转换
-     */
-    public static void timeHandle() {
-        //日期转换为时间戳
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String currentDate = dateFormat.format(new Date());
-        long formatTime = 0;
-        try {
-            formatTime = dateFormat.parse(currentDate).getTime();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        log.info("原日期时间={}，转换后时间戳={}",currentDate,formatTime);
-
-        //时间戳转换为日期
-        long currentTime = System.currentTimeMillis();
-        String formatDate = dateFormat.format(new Date(currentTime));
-        log.info("原时间戳={}，转换后日期时间={}",currentTime,formatDate);
+        TestService.removeList();
     }
 }
---
+++ server-api/src/main/java/com/boilerplate/server/service/TestService.java
@@ -0,0 +1,86 @@
+package com.boilerplate.server.service;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 测试专用service类
+ */
+@Slf4j
+public class TestService {
+    /**
+     * 日期时间格式化与时间戳转换，不能使用SimpleDateFormat有大坑
+     */
+    public static void datetimeHandle() {
+        //common-lang3工具类实现
+        String format = "yyyy-MM-dd HH:mm:ss";
+        long currentTime = System.currentTimeMillis();
+
+        //时间戳转日期时间
+        String datetime = DateFormatUtils.format(new Date(), format);
+        String datetime2 = DateFormatUtils.format(currentTime, format);
+        System.out.println(String.format("最新时间戳=%s，转换日期时间=%s",currentTime,datetime2));
+
+        //时间字符串转unix时间戳，注意：getTime获取的是毫秒时间戳
+        String strDate = "2022-12-01 19:23:44";
+        try {
+            Date parseDate = DateUtils.parseDateStrictly(strDate, format);
+            long unixTimestamp = parseDate.getTime()/1000;
+            System.out.println(String.format("时间字符串=%s，转换时间戳=%s",strDate,unixTimestamp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 字符串处理
+     */
+    public static void stringHandle() {
+        //StringUtils工具类 进行字符串拼接
+        String s1 = null;
+        String s2 = "椰树椰汁·坚持在海南岛用新鲜椰肉鲜榨";
+        System.out.println(StringUtils.join(s1,s2));
+    }
+
+    /**
+     * 如何正确删除list的元素
+     * 禁止在foreach时删除元素，会报错
+     */
+    public static void removeList() {
+        List<String> list = Lists.newArrayList("北京市","海淀区","上地","清河","西二旗","上地","西三旗","西北旺");
+        String delStr = "海淀区";
+        System.out.println("list删除前："+list);
+        //错误方法：禁止在foreach时删除元素，会报错
+        /*for (String s : list) {
+            if (delStr.equals(s)) {
+                list.remove(s);
+            }
+        }*/
+        //正确删除元素 方法1
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            if (delStr.equals(s)) {
+                it.remove();
+            }
+        }
+        //正确删除元素 方法2
+        list = list.stream().filter(f-> !f.contains(delStr)).collect(Collectors.toList());
+        //正确删除元素 方法3（推荐使用）
+        list.removeIf(s-> s.equals(delStr) || s.contains("北京") || s.contains("旗"));
+
+        //list去重
+        list = list.stream().distinct().collect(Collectors.toList());
+        System.out.println("list删除后："+list);
+    }
+    
+}
