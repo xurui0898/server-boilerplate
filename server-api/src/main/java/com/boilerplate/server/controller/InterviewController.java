cd214c6d654b1a02623c501408b613c5d1186fea
眺望流浪的时光 <xurui0898@163.com>
2023-07-18 13:31:52 +0800

    Interview专用

---
+++ server-api/src/main/java/com/boilerplate/server/controller/InterviewController.java
@@ -0,0 +1,18 @@
+package com.boilerplate.server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * 测试专用
+ */
+@RestController
+@Slf4j
+public class InterviewController {
+
+    public static void main(String[] args) {
+        System.out.println("test hhh");
+    }
+}
