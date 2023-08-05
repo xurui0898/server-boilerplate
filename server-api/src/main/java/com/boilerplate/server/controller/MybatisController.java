d024a060bb77cdbbf814de4a10a349071cb6c7b1
眺望流浪的时光 <xurui0898@163.com>
2023-08-05 16:53:35 +0800

    清理文件

---
+++ server-api/src/main/java/com/boilerplate/server/controller/MybatisController.java
@@ -0,0 +1,15 @@
+package com.boilerplate.server.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * mybatis测试
+ */
+@RestController
+@Slf4j
+@RequestMapping("mybatis")
+public class MybatisController {
+
+}
