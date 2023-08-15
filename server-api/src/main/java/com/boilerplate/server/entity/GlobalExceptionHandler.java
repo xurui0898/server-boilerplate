31e9f2bf285c5e7cf54b6d6044e284baaa9a556b
眺望流浪的时光 <xurui0898@163.com>
2023-08-15 19:13:25 +0800

    异常日志记录

--- server-api/src/main/java/com/boilerplate/server/entity/GlobalExceptionHandler.java
+++ server-api/src/main/java/com/boilerplate/server/entity/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.boilerplate.server.entity;
 
 import com.boilerplate.server.enums.ResultCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //声明为异常处理器
 @SuppressWarnings("all")
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     //进行具体异常分类处理
     @ExceptionHandler(Exception.class)
@@ -16,15 +22,18 @@ public class GlobalExceptionHandler {
         if (exception instanceof BindException) {
             //拦截的是validator绑定的异常
             BindException bindException = (BindException) exception;
-            //异常信息
-            String message = bindException.getBindingResult()
-                    .getAllErrors()
-                    .get(0)
-                    .getDefaultMessage();
-            return Response.makeErrRsp(ResultCode.VALID, message);
+            //记录所有错误提示
+            List<FieldError> fieldErrors = bindException.getBindingResult().getFieldErrors();
+            List<String> errorMsg = new ArrayList<>();
+            for (FieldError error : fieldErrors) {
+                errorMsg.add(error.getDefaultMessage());
+            }
+            log.info("参数校验异常="+errorMsg.toString());
+            return Response.makeErrRsp(ResultCode.VALID, errorMsg.get(0));
         }
-        //不属于那两个异常 就返回系统异常错误
-        return Response.makeErrRsp(ResultCode.EXCEPTION, exception.getMessage());
+        //不属于特定异常，就返回系统异常错误
+        log.error("系统异常="+exception.toString());
+        return Response.makeErrRsp(ResultCode.EXCEPTION);
     }
 }
 
