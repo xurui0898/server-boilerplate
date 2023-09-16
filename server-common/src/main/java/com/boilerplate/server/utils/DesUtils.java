f4acc6a88e3065dab6d63f7d82fe5ee1d0dc9422
眺望流浪的时光 <xurui0898@163.com>
2023-09-17 03:05:42 +0800

    Des加解密工具

---
+++ server-common/src/main/java/com/boilerplate/server/utils/DesUtils.java
@@ -0,0 +1,60 @@
+package com.boilerplate.server.utils;
+
+import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.DES;
+import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
+
+/**
+ * Des加解密工具
+ */
+public class DesUtils {
+    public static final String secretKey = "C8RYnYmFEzE=";
+
+    /**
+     * 加密
+     * @param info
+     * @return
+     */
+    public static String encrypt(String info, String secretKey) {
+        byte[] key = Base64.decode(secretKey);
+        DES des = SecureUtil.des(key);
+        return des.encryptHex(info);
+    }
+
+    /**
+     * 解密
+     * @param encrypt
+     * @return
+     */
+    public static String decode(String encrypt, String secretKey) {
+        byte[] key = Base64.decode(secretKey);
+        DES des = SecureUtil.des(key);
+        return des.decryptStr(encrypt);
+    }
+
+    /**
+     * 随机生成一个新密钥，并转为字符串
+     * @return
+     */
+    public static String genSecretKey(){
+        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.DES.getValue()).getEncoded();
+        return Base64.encode(key);
+    }
+
+    public static void main(String[] args) {
+        String secretKey = genSecretKey();
+        System.out.println(secretKey);
+        secretKey = DesUtils.secretKey;
+
+        //加密
+        String str = DesUtils.encrypt("20230916", secretKey);
+        System.out.println(str);
+        String str2 = DesUtils.encrypt("20230917", secretKey);
+        System.out.println(str2);
+
+        // 解密
+        String decode = DesUtils.decode(str, secretKey);
+        System.out.println(decode);
+    }
+}
